c5111b92202670cfbd95f4a0b13c4a2f22377136
jmanwel <new.juang@gmail.com>
2024-01-11 11:48:48 -0300

    [MOD] App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ReactDom from 'react-dom';
 import Posts from "/components/posts.jsx"
 import New_Post from "/components/new_post.jsx"
 import ErrorPage from "./error_page.jsx"
 import CreatePost from "/components/CreatePost.jsx"
+import { Menu } from 'antd';
+import { FormOutlined, EditOutlined } from '@ant-design/icons'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const items = [
+  {
+    label: 'Posts',
+    key: 'posts',
+    icon: <FormOutlined />,
+  },
+  {
+    label: 'Create Post',
+    key: 'app',
+    icon: <EditOutlined />,
+  }
+];
+
 function App(props) {
+  
+  const [current, setCurrent] = useState('posts');
+  
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
   return (
-      <div>
+      <div className='app_container'>
+        <div className='app_main_container'>
+          <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+        </div>
+
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Posts/>}/>
