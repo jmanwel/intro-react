23715ffb66996690b14f8021cf436ac0af9c6c1b
jmanwel <new.juang@gmail.com>
2024-01-19 10:39:42 -0300

    [MOD] sign_in.jsx

--- components/sign_in.jsx
+++ components/sign_in.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 
 const Signin = (props)=>{
 
+    const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState("")
@@ -17,43 +18,41 @@ const Signin = (props)=>{
     
     const navigate = useNavigate();
 
-    // function validateFields() {
-    //     if (email === '' || password === '') {
-    //       // fields are empty, show error message or do something else
-    //       console.log('Fields cannot be empty');
-    //       setValidate({mail_error:"Mail empty" , pass_error: Password empty})
-    //       return false;
-    //     }
-    //     if (field1 === field2) {
-    //       // fields are equal, show error message or do something else
-    //       console.log('Fields cannot be equal');
-    //       return false;
-    //     }
-    //     // fields are valid, continue with form submission or do something else
-    //     console.log('Fields are valid');
-    //     return true;
-    // }
+    function validateFields() {
+        if (email === '' || password === '') {
+          console.log('Fields cannot be empty');
+          setValidate({mail_error:"Mail empty" , pass_error: "Password empty"})
+          return false;
+        }
+        if (email === password) {
+          console.log('Fields cannot be equal');
+          setValidate({mail_error:"Mail equal" , pass_error: "Password equal"})
+          return false;
+        }
+        if (!pattern.test(email)) {
+            console.log("Please enter valid email address.");
+            setValidate({mail_error:"Mail invalid" , pass_error: "-"})
+            return false;
+        }
+        console.log('Fields are valid');
+        return true;
+    }
 
-    // function handleSubmit(e) {
-    //     e.preventDefault();
-    //     if (validateFields()) {
-    //       // do something with form data
-    //       console.log('Form submitted successfully');
-    //     }
-    //   }
 
     const onSignIn = ()=>{
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                console.log("Sign in!!", user)
-                navigate('/posts', { replace: true });
-            })
-            .catch((error) => {
-                setError({code: error.code, msg: error.message})
-                console.log("Error!", error.code, error.message)
-            });
+        if (validateFields()) {
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log("Sign in!!", user)
+                    navigate('/posts', { replace: true });
+                })
+                .catch((error) => {
+                    setError({code: error.code, msg: error.message})
+                    console.log("Error!", error.code, error.message)
+                });
+        }
     }
 
     return (
@@ -85,6 +84,11 @@ const Signin = (props)=>{
             {error &&
                 <p className="error_container">
                     <i class="fa-solid fa-triangle-exclamation"></i> | {error.code} | {error.msg} |
+                </p>
+            }
+            {validate &&
+                <p className="error_container">
+                    <i class="fa-solid fa-triangle-exclamation"></i> | {validate.mail_error} | {validate.pass_error} |
                 </p>
             }
             <div className="container">
